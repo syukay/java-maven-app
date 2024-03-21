f9c41fd846ca61220dc75c93cd4bd7ed938272cc
syukay <135929686+syukay@users.noreply.github.com>
2024-03-21 02:05:45 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
-def buildJar(){
-  echo 'building the application...'
-  sh 'mvn package'
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
 }
 
-def buildImage(){
-  echo 'building the docker image...'
-  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-      sh 'docker build -t syukay/demo-app:jma-2.3 .'
-      sh 'echo $PASS | docker login -u $USER --password-stdin'
-      sh 'docker push syukay/demo-app:jma-2.3'
-  }
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
+    }
 }
 
-def deployApp(){
-  echo 'deploying the application...'
+def deployApp() {
+    echo 'deploying the application...'
 }
 
 return this
