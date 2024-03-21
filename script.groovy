ecdb605b31df9e0aad7a0c18112afb275f7021ca
syukay <135929686+syukay@users.noreply.github.com>
2024-03-21 16:40:28 +1100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,10 +1,15 @@
-def buildApp(){
+def buildJar(){
   echo 'building the application...'
   sh 'mvn package'
 }
 
-def testApp(){
-  echo 'testing the application...'
+def buildImage(){
+  echo 'building the docker image...'
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passowrdVariable: 'PASS', usernameVariable: 'USER')]){
+      sh 'docker build -t syukay/demo-app:jma-2.3 .'
+      sh 'echo $PASS | docker login -u $USER --password-stdin'
+      sh 'docker push syukay/demo-app:jma-2.3'
+  }
 }
 
 def deployApp(){
